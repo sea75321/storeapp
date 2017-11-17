6a065956ec5960d99670e60da1b162e795cbcef7
Steph A <sea75321@gmail.com>
2017-11-17 23:53:20 +0100

    5.2 Add and implement navbar product search box

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
 	has_many :orders
+
+	def self.search(search_term)
+		if Rails.env.production?
+			Product.where("name ILIKE ?", "%#{search_term}%")
+		else
+			Product.where("name LIKE ?", "%#{search_term}%")
+		end
+	end
+
 end
